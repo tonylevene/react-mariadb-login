723be29816c6b2340f81d248222588f8a9a59e53
alexydens <alexydens@outlook.com>
2023-07-29 18:19:53 +0100

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const App = () => {
-  return <div className="text-lg text-slate-500">App</div>;
+  return (
+    <div className="w-full h-full absolute bg-slate-400">
+      <div className="mx-auto max-w-lg bg-slate-200 items-center block">
+        <img
+          src="https://th.bing.com/th?id=OIP.jI93dPHH8N2jeA56aOgrQgHaJ7&w=215&h=289&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
+          width="300"
+          height="700"
+        />
+      </div>
+    </div>
+  );
 };
 
 export default App;
