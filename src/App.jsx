aa51ceb86edae72a1758eac623360405445df6c5
alexydens <alexydens@outlook.com>
2023-07-29 18:47:23 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-// App function
 const App = () => {
+  const [selectedItem, setSelectedItem] = useState("Login");
+
   return (
     <div className="w-full h-full absolute bg-slate-400">
-      <div className="mx-auto max-w-lg bg-slate-200 items-center block">
-        <img
-          src="https://th.bing.com/th?id=OIP.jI93dPHH8N2jeA56aOgrQgHaJ7&w=215&h=289&c=8&rs=1&qlt=90&o=6&dpr=1.3&pid=3.1&rm=2"
-          width="300"
-          height="700"
-        />
+      <div className="mx-auto mt-10 max-w-sm bg-slate-200 items-center block rounded-xl shadow-lg">
+        <div className="top-0 left-0 cursor-pointer select-none">
+          <a
+            onClick={() => setSelectedItem("Login")}
+            className={
+              "float-left w-1/3 text-center rounded-se-xl rounded-ss-xl" +
+              (selectedItem === "Login" ? " bg-slate-300" : "")
+            }
+          >
+            Login
+          </a>
+          <a
+            onClick={() => setSelectedItem("Signup")}
+            className={
+              "float-left w-1/3 text-center rounded-se-xl rounded-ss-xl" +
+              (selectedItem === "Signup" ? " bg-slate-300" : "")
+            }
+          >
+            Signup
+          </a>
+          <a
+            onClick={() => setSelectedItem("Edit Profile")}
+            className={
+              "float-left w-1/3 text-center rounded-se-xl rounded-ss-xl" +
+              (selectedItem === "Edit Profile" ? " bg-slate-300" : "")
+            }
+          >
+            Edit Profile
+          </a>
+        </div>
+        <div className="bg-slate-300 inline-flex rounded-es-xl rounded-ee-xl p-3">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam eos
+          cum nemo, impedit minima qui, soluta quisquam earum tempore quae
+          facilis nisi fugit provident reiciendis, aperiam laboriosam
+          consequatur? Porro, saepe?
+        </div>
       </div>
     </div>
   );
